848343188c579a7e87186b7ce5938f6cb660dd42
tomohiro-iwanaga <tomohiro.iwanaga@change-vision.com>
2020-08-13 18:10:21 +0900

    first commit

---
+++ src/main/java/cv/PositionAdjuster.java
@@ -0,0 +1,22 @@
+package cv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.change_vision.jude.api.inf.presentation.IPresentation;
+
+public class PositionAdjuster {
+    private List<IPresentation> targetPresentations = new ArrayList<>();
+
+    public PositionAdjuster() {
+
+    }
+
+    public void addPresentation() {
+
+    }
+
+    public void adjust() {
+
+    }
+}
